13548ad4a142624b51ca36c766de8f4a14d879bd
Anton Alekseev <Anton Alekseev>
2017-06-11 23:52:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var mongodb = require("mongodb");
+var db;
+var ObjectID = require('mongodb').ObjectID;
+var url = "mongodb://TomKuper:dbpassword1234@ds121212.mlab.com:21212/heroku_w1ckvk1j";
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI||url, function (err, database)
+{
+    if (err)
+    {
+        console.log(err);
+        process.exit(1);
+    }
+
+    db = database;
+    console.log('ok');
+});
+
+app.set('port', (process.env.PORT || 5000));
+
+
+app.get('/', function(request, response) {
+    response.send('All right');
+});
+
+
+
+/*app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});*/
+app.listen(3000);
+
