45676922665dab164955147a9b5deb4ffb2b04d7
ALegenda <adalekseev@edu.hse.ru>
2017-07-06 15:50:31 +0300

    Refactoring code

--- index.js
+++ index.js
@@ -6,173 +6,187 @@ var db;
 var ObjectID = require('mongodb').ObjectID;
 var url = "mongodb://TomKuper:dbpassword1234@ds121212.mlab.com:21212/heroku_w1ckvk1j";
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI || url, function (err, database)
-{
-    if (err)
+mongodb.MongoClient.connect(process.env.MONGODB_URI || url,
+    function (err, database)
     {
-        console.log(err);
-        process.exit(1);
-    }
+        if (err)
+        {
+            console.log(err);
+            process.exit(1);
+        }
 
-    db = database;
-    console.log('ok');
+        db = database;
+        console.log('ok');
 
-});
+    });
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (request, response)
-{
-    response.send('All right');
-});
-
-app.get('/kek', function (request, response)
-{
-    response.send('kek');
-});
-
-app.get('/api/adduser/:login/:password', function (request, response)
-{
-    var collection = db.collection('Users');
-    var login = request.params.login;
-    var tmp = collection.findOne({'login': login}).then(function (doc)
+app.get('/',
+    function (request, response)
     {
-        if (doc)
-        {
-            response.send('this login already exist');
-        }
-        else
-        {
-            var password = request.params.password;
-            var hash = passwordHash.generate((password));
-            collection.insertOne({"login": login, "password": password, "hash": hash, "teams": []});
-            response.send(hash);
-        }
+        response.send('All right');
     });
-});
 
-app.get('/api/addteam/:login/:password', function (request, response)
-{
-    var collection = db.collection('Teams');
-    var login = request.params.login;
-    var tmp = collection.findOne({'login': login}).then(function (doc)
+app.get('/kek',
+    function (request, response)
     {
-        if (doc)
-        {
-            response.send('this login already exist');
-        }
-        else
-        {
-            var password = request.params.password;
-            var hash = passwordHash.generate((password));
-            collection.insertOne({"login": login, "password": password, "hash": hash, "users": []});
-            response.send(hash);
-        }
+        response.send('kek');
     });
-});
 
-app.get('/api/verifyuser/:login/:password', function (request, response)
-{
-    var collection = db.collection('Users');
-    var login = request.params.login;
-    var password = request.params.password;
-    var tmp = collection.findOne({'login': login}).then(function (doc)
+app.get('/api/adduser/:login/:password',
+    function (request, response)
     {
-        if (doc)
-        {
-            console.log('kek');
-            console.log(doc.hash);
-
-            if (passwordHash.verify(password, doc.hash))
-                response.send(doc.hash);
-            else
-                response.send('Incorrect password');
-        }
-        else
-        {
-            response.send('I have not this user');
-        }
-    });
-});
-
-app.get('/api/userinfo/:userlogin', function (request, response)
-{
-    var collection = db.collection('Tranzactions');
-    var login = request.params.userlogin;
-    var tmp = collection.find({'user': login}, function (err, items)
-    {
-        response.send(items)
-        /*
-        if (items)
-        {
-            response.send(items);
-        }
-        else
-        {
-            response.send(items);
-        }*/
-    });
-});
-
-app.get('/api/verifyteam/:login/:password', function (request, response)
-{
-    var collection = db.collection('Teams');
-    var login = request.params.login;
-    var password = request.params.password;
-    var tmp = collection.findOne({'login': login}).then(function (doc)
-    {
-        if (doc)
-        {
-            if (passwordHash.verify(password, doc.hash))
-                response.send(doc.hash);
-            else
-                response.send('Incorrect password');
-        }
-        else
-        {
-            response.send('I have not this team');
-        }
-    });
-});
-
-app.get('/api/addusertoteam/:user/:hashpass/:team/:hashteam', function (request, response)
-{
-    var collectionUser = db.collection('Users');
-    var collectionTeam = db.collection('Teams');
-    collectionUser.findOne({'login': request.params.user, 'hash': request.params.hashpass}, function (err, item)
-    {
-        if (item)
-        {
-            collectionTeam.findOne({'login': request.params.team, 'hash': request.params.hashteam},
-                function (err2, item2)
+        var collection = db.collection('Users');
+        var login = request.params.login;
+        var tmp = collection.findOne({'login': login}).then(
+            function (doc)
+            {
+                if (doc)
                 {
-                    if (item2)
-                    {
-                        var u = item2.users;
-                        if (!(u.indexOf(item.login) > -1))
-                            u.push(item.login);
-                        var t = item.teams;
-                        if (!(t.indexOf(item2.login) > -1))
-                            t.push(item2.login);
-                        collectionTeam.updateOne({'login': request.params.team}, {$set: {'users': u}});
-                        collectionUser.updateOne({'login': request.params.user}, {$set: {'teams': t}});
-
-                        response.send('ok');
-                    }
-                    else
-                        response.send('I have not this team');
-                });
-        }
-        else
-            response.send('I have not this user');
+                    response.send('this login already exist');
+                }
+                else
+                {
+                    var password = request.params.password;
+                    var hash = passwordHash.generate((password));
+                    collection.insertOne({"login": login, "password": password, "hash": hash, "teams": []});
+                    response.send(hash);
+                }
+            });
     });
 
-});
+app.get('/api/addteam/:login/:password',
+    function (request, response)
+    {
+        var collection = db.collection('Teams');
+        var login = request.params.login;
+        var tmp = collection.findOne({'login': login}).then(
+            function (doc)
+            {
+                if (doc)
+                {
+                    response.send('this login already exist');
+                }
+                else
+                {
+                    var password = request.params.password;
+                    var hash = passwordHash.generate((password));
+                    collection.insertOne({"login": login, "password": password, "hash": hash, "users": []});
+                    response.send(hash);
+                }
+            });
+    });
 
-app.listen(app.get('port'), function ()
-{
-    console.log('Node app is running on port', app.get('port'));
-});
+app.get('/api/verifyuser/:login/:password',
+    function (request, response)
+    {
+        var collection = db.collection('Users');
+        var login = request.params.login;
+        var password = request.params.password;
+        var tmp = collection.findOne({'login': login}).then(
+            function (doc)
+            {
+                if (doc)
+                {
+                    console.log('kek');
+                    console.log(doc.hash);
+
+                    if (passwordHash.verify(password, doc.hash))
+                        response.send(doc.hash);
+                    else
+                        response.send('Incorrect password');
+                }
+                else
+                {
+                    response.send('I have not this user');
+                }
+            });
+    });
+
+app.get('/api/userinfo/:userlogin',
+    function (request, response)
+    {
+        var collection = db.collection('Tranzactions');
+        var login = request.params.userlogin;
+        var tmp = collection.find({'user': login},
+            function (err, items)
+            {
+                if (items)
+                {
+                    response.send(items);
+                }
+                else
+                {
+                    response.send(login)
+                }
+            });
+    });
+
+app.get('/api/verifyteam/:login/:password',
+    function (request, response)
+    {
+        var collection = db.collection('Teams');
+        var login = request.params.login;
+        var password = request.params.password;
+        var tmp = collection.findOne({'login': login}).then(
+            function (doc)
+            {
+                if (doc)
+                {
+                    if (passwordHash.verify(password, doc.hash))
+                        response.send(doc.hash);
+                    else
+                        response.send('Incorrect password');
+                }
+                else
+                {
+                    response.send('I have not this team');
+                }
+            });
+    });
+
+app.get('/api/addusertoteam/:user/:hashpass/:team/:hashteam',
+    function (request, response)
+    {
+        var collectionUser = db.collection('Users');
+        var collectionTeam = db.collection('Teams');
+        collectionUser.findOne({'login': request.params.user, 'hash': request.params.hashpass},
+            function (err, item)
+            {
+                if (item)
+                {
+                    collectionTeam.findOne({'login': request.params.team, 'hash': request.params.hashteam},
+                        function (err2, item2)
+                        {
+                            if (item2)
+                            {
+                                var u = item2.users;
+                                if (!(u.indexOf(item.login) > -1))
+                                    u.push(item.login);
+                                var t = item.teams;
+                                if (!(t.indexOf(item2.login) > -1))
+                                    t.push(item2.login);
+                                collectionTeam.updateOne({'login': request.params.team}, {$set: {'users': u}});
+                                collectionUser.updateOne({'login': request.params.user}, {$set: {'teams': t}});
+
+                                response.send('ok');
+                            }
+                            else
+                                response.send('I have not this team');
+                        });
+                }
+                else
+                    response.send('I have not this user');
+            });
+
+    });
+
+app.listen(app.get('port'),
+    function ()
+    {
+        console.log('Node app is running on port', app.get('port'));
+    });
 
 //app.listen(3000);
 
